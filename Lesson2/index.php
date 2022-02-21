3d4b79509cb90985ebaf6609cb9f8b068db3e1de
EduardMoiseenko <Prorok87@gmail.com>
2022-02-21 15:24:23 +0200

    Update index.php
    Пройден 1 урок курса

--- Lesson2/index.php
+++ Lesson2/index.php
@@ -1,1 +1,24 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Урок 1</title>
+</head>
+<body>
+  <p>Привет мир!</p>
+  <?php
+  //однострочный комментарий
+  #также однострочный комментарий
+  /*это
+  многострочный
+  комментарий. Здесь не будут выполняться комманды
+  */
+  echo 'Привет чувак!';
+  print '<p>Hello!</p>';
+
+  ?>
+</body>
+</html>
