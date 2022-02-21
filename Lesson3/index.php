328956eb684bbb670322acc4a08c67d4c86c0637
EduardMoiseenko <Prorok87@gmail.com>
2022-02-22 01:38:45 +0200

    PHP переменные и константы

---
+++ Lesson3/index.php
@@ -0,0 +1,32 @@
+<?php
+error_reporting(-1);
+
+$var = 'Hello world';
+$theVar = 'Winnie';
+$fruit = 'apple';
+$winnie_pooh = "Hello, I'm Winnie. I have 2 {$fruit}s";
+$var = '123';
+define("PAGE","new page");
+//define("PAGE","new page2");
+
+const PAGE2 = 'new const'
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Урок 2</title>
+</head>
+<body>
+<h1><?php echo $var; ?></h1>
+<h1><?php echo $theVar; ?></h1>
+<h1><?php echo $winnie_pooh; ?></h1>
+<h1><?php echo PAGE ?></h1>
+<h1><?php echo PAGE2 ?></h1>
+
+</body>
+</html>
