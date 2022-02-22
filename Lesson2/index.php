58d77dadc30e22bd0e792b4de9494094e3a72add
EduardMoiseenko <Prorok87@gmail.com>
2022-02-22 15:07:00 +0200

    Update index.php
    <title>Урок 1</title>

--- Lesson2/index.php
+++ Lesson2/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Урок 1</title>
+    <title>Урок 2</title>
 </head>
 <body>
   <p>Привет мир!</p>
